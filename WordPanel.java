216b2e8831324af58df7b6de5fbef590a6dd6a30
DavidByte <praemoveo@gmail.com>
2021-09-04 19:08:11 +0200

    impletemented threading in wordPanel

--- WordPanel.java
+++ WordPanel.java
@@ -13,8 +13,8 @@ public class WordPanel extends JPanel implements Runnable
 		private WordRecord[] words;
 		private int noWords;
 		private int maxY;
-
-		
+		private int count;
+    	public static AtomicBoolean paused;
 
 		
 		public void paintComponent(Graphics g) 
@@ -37,16 +37,85 @@ public class WordPanel extends JPanel implements Runnable
 		   
 		  }
 		
-		WordPanel(WordRecord[] words, int maxY) {
-			this.words=words; //will this work?
-			noWords = words.length;
-			done=false;
-			this.maxY=maxY;		
-		}
-		
-		public void run() {
-			//add in code to animate this
-		}
+		 /**
+     * Parameterised Constructor
+     *
+     * @param words
+     * @param maxY
+     */
+    WordPanel(WordRecord[] words, int maxY) 
+	{
+        this.words = words; 
+        noWords = words.length;
+        done = false;
+        this.maxY = maxY;
+        paused  = new AtomicBoolean(false);
+    }
+
+    /**
+     * Run method
+     */
+    public void run() 
+	{
+
+        done = false;
+        WordRecord word = words[count];
+        //System.out.println(word.getWord());
+        count++;
+
+        while (!done) 
+		{
+
+            if(WordApp.score.getTotal() == WordApp.totalWords)
+			{
+                done = true;
+                Completion.done = true;
+                word.resetWord();
+                break;
+            }
+
+            if (!paused.get()) 
+			{
+            word.drop(10);
+
+            try 
+			{
+                Thread.sleep(word.getSpeed());
+            } 
+			catch (InterruptedException e) 
+			{
+                e.printStackTrace();
+            }
+            repaint();
+
+            if (word.dropped()) 
+			{
+                word.resetWord();
+                WordApp.score.missedWord();
+                WordApp.updateScores();
+            }
+
+            String text = WordApp.text;
+            if(text.equals(word.getWord()))
+			{
+                WordApp.score.caughtWord(word.getWord().length());
+                word.resetWord();
+                WordApp.updateScores();
+            }
+
+            if (done) 
+			{
+                word.resetWord();
+                WordApp.score.resetScore();
+                WordApp.updateScores();
+                count = 0;
+            }
+
+            }
+
+        }
+    }
+}
 
 	}
 
